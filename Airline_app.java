04784cabfc541ee60f689b310c9554d41a504cb1
Ghost49-del <53409725+Ghost49-del@users.noreply.github.com>
2023-02-10 06:36:28 +0200

    Update Airline_app.java

--- Airline_app.java
+++ Airline_app.java
@@ -1,9 +1,7 @@
-/* HelloWorld.java
- */
+public class Main {
 
-public class HelloWorld
-{
-	public static void main(String[] args) {
-		System.out.println("Hello World!");
-	}
+  public static void main(String[] args) {
+  	System.out.println("Hello, World!"); 
+  }
+  
 }
